fb088a755fdfffab10e9037969050bb2d6939fc7
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2017-12-12 13:11:15 -0800

    add events migrations

--- migrations/2017-12-10-011200_guild_config/up.sql
+++ migrations/2017-12-10-011200_guild_config/up.sql
@@ -1,9 +1,9 @@
 -- Your SQL goes here
-
 CREATE TABLE guilds (
     id bigint PRIMARY KEY,
     name TEXT NOT NULL,
     join_msg TEXT,
+    join_react TEXT,
     leave_msg TEXT,
     invite_guard BOOLEAN,
     log_msg bigint,
