828218bfb597d16e71c4b4e4f1ea53f761989948
drklee3 <dlee3@scu.edu>
2017-12-12 13:11:55 -0800

    add log_events and get_events methods

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,9 @@
 use diesel;
+use diesel::result::Error;
 use diesel::pg::PgConnection;
+use diesel::QueryDsl;
 use diesel::RunQueryDsl;
+use diesel::ExpressionMethods;
 
 use r2d2::Pool;
 use r2d2_diesel::ConnectionManager;
@@ -10,6 +13,8 @@ use std::sync::Arc;
 
 use serenity::model::Guild;
 use models::NewGuildConfig;
+use models::EventCounter;
+use models::NewEventCounter;
 
 pub struct ConnectionPool {
     pool: Arc<Pool<ConnectionManager<PgConnection>>>,
@@ -29,6 +34,8 @@ pub fn init() -> ConnectionPool {
 
 
 impl ConnectionPool {
+    /// Creates a new config for a guild,
+    /// ie when the bot joins a new guild.
     pub fn new_guild<'a>(&self, guild: &'a Guild) {
         use schema::guilds;
 
@@ -49,5 +56,50 @@ impl ConnectionPool {
             .values(&new_guild_obj)
             .execute(&*conn)
             .expect("Error saving new guild.");
+    }
+
+    /// Logs a counter for each event that is handled
+    pub fn log_event(&self, event_name: String) {
+        use schema::events;
+        use schema::events::dsl::*;
+
+        // get a connection from the pool
+        let conn = (*&self.pool).get().unwrap();
+
+        // fetch event
+        let rows = events
+            .filter(name.eq(&event_name))
+            .load::<EventCounter>(&*conn)
+            .expect("Error loading event");
+
+        // check if a row was found
+        if rows.len() == 1 {
+            // increment the counter
+            diesel::update(events).set(count.eq(rows[0].count + 1));
+        } else {
+            let new_event_obj = NewEventCounter {
+                name: &event_name,
+                count: 1,
+            };
+
+            diesel::insert_into(events::table)
+                .values(&new_event_obj)
+                .execute(&*conn)
+                .expect("Error saving new event.");
+        }
+    }
+
+    /// Gets the counters for events handled
+    pub fn get_events(&self) -> Result<Vec<EventCounter>, Error> {
+        use schema::events::dsl::*;
+
+        // get a connection from the pool
+        let conn = (*&self.pool).get().unwrap();
+
+        let results = events.load::<EventCounter>(&*conn).expect(
+            "Error loading events",
+        );
+
+        Ok(results)
     }
 }
