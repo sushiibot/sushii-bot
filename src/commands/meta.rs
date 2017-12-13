a64da65d7866445846d167c433e08fe5a311a11e
drklee3 <dlee3@scu.edu>
2017-12-12 18:26:28 -0800

    add events command

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,3 +1,8 @@
+use serenity::framework::standard::CommandError;
+
+use std::fmt::Write;
+use database;
+
 command!(latency(ctx, msg) {
     let latency = ctx.shard.lock()
         .latency()
@@ -6,4 +11,21 @@ command!(latency(ctx, msg) {
         });
 
     let _ = msg.channel_id.say(latency);
+});
+
+command!(events(ctx, msg) {
+    let mut data = ctx.data.lock();
+    let pool = data.get_mut::<database::ConnectionPool>().unwrap();
+
+    if let Ok(events) = pool.get_events() {
+        for event in events {
+            let mut s = "```ruby\n".to_string();
+            let _ = write!(s, "{}: {}\n", event.name, event.count);
+            let _ = write!(s, "```");
+
+            let _ = msg.channel_id.say(&s);
+        }
+    } else {
+        return Err(CommandError("Failed to get events.".to_string()));
+    }
 });
