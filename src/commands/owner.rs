95fb7760ead10d154e2071a2fbd9dea8f3191dee
drklee3 <dlee3@scu.edu>
2017-12-15 17:24:42 -0800

    add username command to change bot username

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use serenity::framework::standard::CommandError;
+
 command!(quit(ctx, msg, _args) {
     match ctx.quit() {
         Ok(()) => {
@@ -6,5 +9,21 @@ command!(quit(ctx, msg, _args) {
         Err(why) => {
             let _ = msg.reply(&format!("Failed to shutdown: {:?}", why));
         },
+    }
+});
+
+command!(username(ctx, msg, args) {
+    let name = match args.single::<String>() {
+        Ok(val) => val,
+        Err(_) => {
+            return Err(CommandError("Missing argument".to_owned()));
+        },
+    };
+
+    match ctx.edit_profile(|e| e.username(&name)) {
+        Ok(_) => {
+            let _ = msg.channel_id.say(&format!("Changed my username to {}", &name));
+        },
+        Err(e) => return Err(CommandError(e.description().to_owned())),
     }
 });
