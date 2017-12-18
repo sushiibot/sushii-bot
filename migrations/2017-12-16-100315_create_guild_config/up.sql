dc439a114ff66a11c827e647b161ec0b1e663310
drklee3 <dlee3@scu.edu>
2017-12-18 12:14:23 -0800

    make capitalizations consistent

--- migrations/2017-12-16-100315_create_guild_config/up.sql
+++ migrations/2017-12-16-100315_create_guild_config/up.sql
@@ -1,12 +1,12 @@
 -- Your SQL goes here
 CREATE TABLE guilds (
-    id bigint PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     name TEXT,
     join_msg TEXT,
     join_react TEXT,
     leave_msg TEXT,
     invite_guard BOOLEAN,
-    log_msg bigint,
-    log_mod bigint,
+    log_msg BIGINT,
+    log_mod BIGINT,
     prefix TEXT
 )
