147ba49919a950cb968f574116efdf040903e07d
drklee3 <dlee3@scu.edu>
2017-12-18 15:40:44 -0800

    add reminder and reminders commands

--- src/commands/misc.rs
+++ src/commands/misc.rs
@@ -1,6 +1,11 @@
 use serenity::framework::standard::CommandError;
 use reqwest;
 use reqwest::header::ContentType;
+use regex::Regex;
+use std::fmt::Write;
+
+use chrono::{DateTime, Utc, Duration};
+use database;
 
 #[derive(Deserialize)]
 struct Response {
@@ -48,5 +53,90 @@ command!(play(_ctx, msg, args) {
         Err(e) => {
             let _ = msg.channel_id.say(format!("Error: {}", e));
         }
+    }
+});
+
+
+command!(reminder(ctx, msg, args) {
+    let time = match args.single::<String>() {
+        Ok(val) => val,
+        Err(_) => return Err(CommandError("Not enough arguments".to_owned())),
+    };
+
+    // parse durations for each
+    let re = Regex::new(r"(\d+)\s*d").unwrap();
+    let day = match re.find(&time) {
+        Some(val) => val.as_str().replace("d", "").parse::<i64>().unwrap(),
+        None => 0
+    };
+    
+    let re = Regex::new(r"(\d+)\s*h").unwrap();
+    let hour = match re.find(&time){
+        Some(val) => val.as_str().replace("h", "").parse::<i64>().unwrap(),
+        None => 0
+    };
+    
+    let re = Regex::new(r"(\d+)\s*m").unwrap();
+    let min = match re.find(&time) {
+        Some(val) => val.as_str().replace("m", "").parse::<i64>().unwrap(),
+        None => 0
+    };
+    
+    let re = Regex::new(r"(\d+)\s*s").unwrap();
+    let sec = match re.find(&time) {
+        Some(val) => val.as_str().replace("s", "").parse::<i64>().unwrap(),
+        None => 0
+    };
+
+    // get current time and add up the offsets
+    let now: DateTime<Utc> = Utc::now();
+    let offset = Duration::days(day) + Duration::hours(hour) + Duration::minutes(min) + Duration::seconds(sec);
+
+    // get the reminder time, current time + offset time
+    let remind_date = now + offset;
+    let remind_date = remind_date.naive_utc();
+
+    // check if offset is great enough
+    if offset.num_minutes() < 1 {
+        return Err(CommandError("Reminder must be at least 1 minute from now".to_owned()));
+    }
+
+    let content = args.full();
+
+    // throw reminder into the database
+    let mut data = ctx.data.lock();
+    let pool = data.get_mut::<database::ConnectionPool>().unwrap();
+
+    pool.add_reminder(msg.author.id.0, &content, &remind_date);
+
+    let s = format!("I'll remind you at `{}` to `{}`", remind_date.format("%Y-%m-%d %H:%M:%S UTC"), content);
+    let _ = msg.channel_id.say(&s);
+});
+
+command!(reminders(ctx, msg, _args) {
+    // throw reminder into the database
+    let mut data = ctx.data.lock();
+    let pool = data.get_mut::<database::ConnectionPool>().unwrap();
+
+    let reminders = pool.get_reminders(msg.author.id.0);
+
+    if let Some(reminders) = reminders {
+        let mut s = format!("You have {} reminders:\n```rust", reminders.len());
+
+        for reminder in reminders {
+            let _ = write!(s, "{} - {}\n", reminder.time_to_remind.format("%Y-%m-%d %H:%M:%S UTC"), reminder.description);
+        }
+
+        // get current timestamp
+        let utc: DateTime<Utc> = Utc::now();
+        let now = utc.naive_utc();
+
+        let _ = write!(s, "\nCurrent time: {}\n", now.format("%Y-%m-%d %H:%M:%S UTC"));
+
+        let _ = write!(s, "```");
+
+        let _ = msg.channel_id.say(&s);
+    } else {
+        let _ = msg.channel_id.say("You have no reminders set.");
     }
 });
