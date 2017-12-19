de3ccdee8b8c21ba4137edca1afd999d3574f206
drklee3 <dlee3@scu.edu>
2017-12-18 21:25:56 -0800

    add util function to help get user ids

---
+++ src/util.rs
@@ -0,0 +1,25 @@
+use chrono::{DateTime, Utc};
+use chrono::naive::NaiveDateTime;
+
+pub fn get_id(value: &str) -> Option<u64> {
+    // check if it's already an ID
+    if let Ok(id) = value.parse::<u64>() {
+        return Some(id);
+    }
+
+    // Derived from https://docs.rs/serenity/0.4.5/src/serenity/utils/mod.rs.html#158-172
+    if value.starts_with("<@!") {
+        let len = value.len() - 1;
+        value[3..len].parse::<u64>().ok()
+    } else if value.starts_with("<@") {
+        let len = value.len() - 1;
+        value[2..len].parse::<u64>().ok()
+    } else {
+        None
+    }
+}
+
+pub fn get_now_utc() -> NaiveDateTime {
+    // get current timestamp
+    Utc::now().naive_utc()
+}
