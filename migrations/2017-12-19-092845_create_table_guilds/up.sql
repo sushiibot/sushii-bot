2ff2685ffd408383fbdb3e29a35b5bb5e45252d8
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2017-12-19 01:43:22 -0800

    [database] update guild configs for channel for member join/leave messages, member log channel, and mute role

--- migrations/2017-12-19-092845_create_table_guilds/up.sql
+++ migrations/2017-12-19-092845_create_table_guilds/up.sql
@@ -5,8 +5,11 @@ CREATE TABLE guilds (
     join_msg TEXT,
     join_react TEXT,
     leave_msg TEXT,
+    msg_channel BIGINT,
     invite_guard BOOLEAN,
     log_msg BIGINT,
     log_mod BIGINT,
+    log_member BIGINT,
+    mute_role BIGINT,
     prefix TEXT
 )
