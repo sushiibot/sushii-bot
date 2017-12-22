43b200616f7d774be3085942c0ae5a408651c05b
drklee3 <dlee3@scu.edu>
2017-12-21 21:23:01 -0800

    use local webserver to generate level image

--- src/commands/levels.rs
+++ src/commands/levels.rs
@@ -1,9 +1,13 @@
 use serenity::framework::standard::CommandError;
-
+use reqwest;
+use reqwest::header::ContentType;
 use std::fmt::Write;
+use std::collections::HashMap;
 use database;
 
 use util;
+
+const LEVEL_HTML: &'static str = include_str!("../../html/rank.html");
 
 command!(rank(ctx, msg, args) {
     let mut data = ctx.data.lock();
@@ -37,5 +41,34 @@ command!(rank(ctx, msg, args) {
     let _ = write!(s, "Last Message: {}\n", level_data.last_msg.format("%Y-%m-%d %H:%M:%S UTC"));
     let _ = write!(s, "```");
 
-    let _ = msg.channel_id.say(&s);
+    let mut html = LEVEL_HTML.clone();
+
+    let html = html.replace("{DAILY}", &level_data.msg_day.to_string());
+    let html = html.replace("{WEEKLY}", &level_data.msg_week.to_string());
+    let html = html.replace("{MONTHLY}", &level_data.msg_month.to_string());
+    let html = html.replace("{ALL}", &level_data.msg_all_time.to_string());
+    let html = html.replace("{LAST_MESSAGE}", &level_data.last_msg.format("%Y-%m-%d %H:%M:%S UTC").to_string());
+
+
+    let mut json = HashMap::new();
+    json.insert("html", html);
+    json.insert("width", "400".to_owned());
+    json.insert("height", "300".to_owned());
+
+    let client = reqwest::Client::new();
+    let res = client.post("http://127.0.0.1:3000/html")
+        .json(&json)
+        .send()?.error_for_status();
+
+    let mut img = match res {
+        Ok(val) => val,
+        Err(e) => return Err(CommandError(format!("Error: {}", e))),
+    };
+
+    let mut buf: Vec<u8> = vec![];
+    img.copy_to(&mut buf)?;
+
+    let files = vec![(&buf[..], "level.png")];
+
+    let _ = msg.channel_id.send_files(files, |m| m.content(&s));
 });
