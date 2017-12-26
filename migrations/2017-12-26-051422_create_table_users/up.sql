1a7895817d7ffd33eaedd39d6de44aee7f836c59
drklee3 <dlee3@scu.edu>
2017-12-25 23:51:37 -0800

    add create_table_users migration

---
+++ migrations/2017-12-26-051422_create_table_users/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY,
+    last_msg TIMESTAMP NOT NULL,
+    msg_activity INTEGER[] NOT NULL,
+    rep INTEGER NOT NULL,
+    last_rep TIMESTAMP
+)
