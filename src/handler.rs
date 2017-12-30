ed45d9777525fc30d1ef0d37e990920867964a93
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2017-12-30 02:51:58 -0800

    add handler for mod_log

--- src/handler.rs
+++ src/handler.rs
@@ -64,7 +64,9 @@ impl EventHandler for Handler {
         update_event(&ctx, "GUILD_BAN_ADD");
     }
 
-    fn on_guild_ban_removal(&self, ctx: Context, _: GuildId, _: User) {
+    fn on_guild_ban_removal(&self, ctx: Context, guild: GuildId, user: User) {
+        // exec_on_guild_ban_removal!([&ctx, &guild, &user], mod_log);
+
         update_event(&ctx, "GUILD_BAN_REMOVE");
     }
 
