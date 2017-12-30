566f3135025f8f9407325c8a8b2529f4d9422060
drklee3 <dlee3@scu.edu>
2017-12-29 18:31:44 -0800

    add create_table_mod_log migration

---
+++ migrations/2017-12-29-235810_create_table_mod_log/up.sql
@@ -0,0 +1,13 @@
+-- Your SQL goes here
+CREATE TABLE mod_log (
+    id SERIAL PRIMARY KEY,
+    case_id INTEGER NOT NULL,
+    guild_id BIGINT NOT NULL,
+    executor_id BIGINT,
+    user_id BIGINT NOT NULL,
+    user_tag TEXT NOT NULL,
+    action TEXT NOT NULL,
+    reason TEXT,
+    action_time TIMESTAMP NOT NULL,
+    msg_id BIGINT
+)
