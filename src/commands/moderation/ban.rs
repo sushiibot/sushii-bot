a98f26b6d1ff9f60d990c0e5cc3913807bae60fd
drklee3 <dlee3@scu.edu>
2018-01-03 03:00:14 -0800

    add bulk ban capabilities to ban command

--- src/commands/moderation/ban.rs
+++ src/commands/moderation/ban.rs
@@ -6,13 +6,22 @@ use serenity::Error;
 use serenity::model::ModelError::InvalidPermissions;
 use serenity::model::ModelError::DeleteMessageDaysAmount;
 
+use regex::Regex;
+use std::fmt::Write;
+
 command!(ban(ctx, msg, args) {
     // get the target
-    let user = args.single::<String>()?;
-    let user_id = match get_id(&user) {
-        Some(val) => val,
-        None => return Err(CommandError::from("Missing mention or ID.")),
-    };
+    let raw_users = args.single::<String>()?;
+    let split = raw_users.split(",");
+    let mut users = Vec::new();
+
+    // loop through each one and parse the user id
+    for user in split {
+        match get_id(&user) {
+            Some(val) => users.push(val),
+            None => return Err(CommandError::from("Malformed mention or ID.")),
+        };
+    }
 
     // get the guild
     let guild = match msg.guild() {
@@ -24,32 +33,73 @@ command!(ban(ctx, msg, args) {
     let reason_raw = args.full();
     let reason;
 
-    if reason_raw.is_empty() {
-        reason = None;
-    } else {
+    let re = Regex::new(r"\d{17,18}[a-zA-Z ]+").unwrap();
+    if re.is_match(&reason_raw) && !reason_raw.starts_with("\\") {
+        return Err(CommandError::from("There seems to be a user ID in the beginning of your reason. \
+                If you're banning multiple users at once, be sure to not leave spaces between the commas, IDs or mentions.  \
+                If you actually wanted an ID in your reason, prefix your reason with a backslash (\\\\):\n\
+                Example: `-ban 12345678910,456789123 \\10987654321 is his best friend but he's really smelly`"));
+    } else if reason_raw.starts_with("\\") && !reason_raw.is_empty() {
+        reason = Some(&reason_raw[1..]);
+    } else if !reason_raw.is_empty() {
         reason = Some(&reason_raw[..]);
+    } else {
+        reason = None;
     }
-
-    // fetch the user for tag
-    let user = match UserId(user_id).get() {
-        Ok(val) => val,
-        Err(e) => return Err(CommandError::from(format!("Failed to fetch user: {}", e))),
-    };
-
-    // ban the user
-    let _ = match guild.ban(user_id, 7) {
-        Err(Error::Model(InvalidPermissions(permissions))) => 
-            return Err(CommandError::from(format!("I don't have permission to ban this user, requires: `{:?}`.", permissions))),
-        Err(Error::Model(DeleteMessageDaysAmount(num))) => 
-            return Err(CommandError::from(format!("The number of days worth of messages to delete is over the maximum: ({}).", num))),
-        Err(_) => return Err(CommandError::from("There was an error trying to ban this user.")),
-        _ => {},
-    };
 
     // log the ban in the database
     let pool = get_pool(&ctx);
-    let _ = pool.add_mod_action("ban", guild.id.0, &user, reason, true);
+    let mut s = String::new();
 
-    let s = format!("Banned user {} ({}).", user.tag(), user.id.0);
+    let len = users.len();
+
+    // more than 1 ban so let's use a code block
+    if len > 1 {
+        let _ = write!(s, "```ruby\n");
+    }
+
+    let _ = write!(s, "Attempted to ban {} users:\n\n", users.len());
+
+    for u in users {
+        // fetch the user for tag
+        let user = match UserId(u).get() {
+            Ok(val) => val,
+            Err(e) => {
+                let _ = write!(s, "{} - Error: Failed to fetch user: {}\n", u, &e);
+                continue;
+            }
+        };
+
+        // format a tag (id) string for the user
+        let user_tag_id = format!("{} ({})", user.tag(), user.id.0);
+
+        // ban the user
+        let _ = match guild.ban(u, 7) {
+            Err(Error::Model(InvalidPermissions(permissions))) => {
+                let e = format!("I don't have permission to ban this user, requires: `{:?}`.", permissions);
+                let _ = write!(s, "{} - Error: {}\n", &user_tag_id, &e);
+            },
+            Err(Error::Model(DeleteMessageDaysAmount(num))) => {
+                let e = format!("The number of days worth of messages to delete is over the maximum: ({}).", num);
+                let _ = write!(s, "{} - Error: {}\n", &user_tag_id, &e);
+            }
+            Err(_) => {
+                let e = "There was an unknown error trying to ban this user.";
+                let _ = write!(s, "{} - Error: {}\n", &user_tag_id, &e);
+            },
+            Ok(_) => {
+                // add the ban to the database if it was successfull
+                // THIS IS BEING ADDED AFTER THE BAN TAKEN PLACE, SO BAN HANDLER NEVER SEES THIS
+                let _ = pool.add_mod_action("ban", guild.id.0, &user, reason, true);
+                let _ = write!(s, "{} - Successfully banned.\n", &user_tag_id);
+            },
+        };
+    }
+
+    // close the code block if > 1 ban
+    if len > 1 {
+        let _ = write!(s, "```");
+    }
+    
     let _ = msg.channel_id.say(&s);
 });
