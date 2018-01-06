6a8dcc2b88b1d57ff010885385d3fd05358df463
drklee3 <dlee3@scu.edu>
2018-01-05 18:03:36 -0800

    add role channel and config

--- migrations/2017-12-19-092845_create_table_guilds/up.sql
+++ migrations/2017-12-19-092845_create_table_guilds/up.sql
@@ -6,6 +6,8 @@ CREATE TABLE guilds (
     join_react TEXT,
     leave_msg TEXT,
     msg_channel BIGINT,
+    role_channel BIGINT,
+    role_config JSONB,
     invite_guard BOOLEAN,
     log_msg BIGINT,
     log_mod BIGINT,
