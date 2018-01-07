a1efaa906538e77cf672515e891b5f7557d1623f
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-01-06 20:50:21 -0800

    add local helper macro

---
+++ src/utils/locale.rs
@@ -0,0 +1,41 @@
+use serde_json;
+use serde_json::Value;
+
+lazy_static! {
+    pub static ref LOCALE: Value = init_locale();
+}
+
+
+fn init_locale() -> Value {
+    let locale_file = include_str!("../../assets/locale.json");
+
+    serde_json::from_str(&locale_file).expect("Failed to parse locale JSON")
+}
+
+macro_rules! get_msg {
+    ($ptr:expr $(, $replace:expr )* ) => {
+        {
+            use utils::locale::LOCALE;
+
+            let default = format!(
+                "Unknown error or info message, please fix in local.json `({})`",
+                $ptr
+            );
+
+            let mut s = match LOCALE.pointer(&["/", $ptr].join("")) {
+                Some(val) => match val.as_str() {
+                    Some(string) => string.to_owned(),
+                    None => default,
+                },
+                None => default,
+            };
+
+            $(
+                s = s.replace("{}", $replace);
+            )*
+
+
+            s
+        }
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,3 +2,5 @@ pub mod user;
 pub mod time;
 pub mod config;
 pub mod num;
+#[macro_use]
+pub mod locale;
