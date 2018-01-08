1ed3764f99d038670ab1e760f60c30e065e284f4
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-01-07 17:21:52 -0800

    add anti spam plugin

---
+++ src/plugins/anti_spam.rs
@@ -0,0 +1,70 @@
+use serenity::model::Message;
+use serenity::prelude::Context;
+use serenity::CACHE;
+
+use utils::config::get_pool;
+
+pub fn on_message(ctx: &Context, msg: &Message) {
+    if let Some(guild) = msg.guild() {
+        let guild = guild.read().unwrap();
+
+        let current_user_id = CACHE.read().unwrap().user.id;
+
+        // return if bot doesn't have role perms
+        if !guild.member_permissions(current_user_id).manage_roles() {
+            return;
+        }
+
+        // return if bot sent the message, not sure why this would happen
+        if msg.author.id == current_user_id {
+            return;
+        }
+
+        // allow those with perms to bypass
+        if guild.member_permissions(msg.author.id).manage_guild() {
+            return;
+        }
+
+        // currently 10, add configurable options later
+        if msg.mentions.len() > 10 {
+            // get the member
+            let mut member = match guild.member(msg.author.id) {
+                Ok(val) => val,
+                Err(e) => {
+                    error!("Error while fetching member: {}", e);
+                    return;
+                }
+            };
+
+            let pool = get_pool(&ctx);
+
+            // get the mute role
+            let mute_role = match pool.get_guild_config(guild.id.0).mute_role {
+                Some(val) => val,
+                None => return,
+            };
+
+            // create a pending case with mute, reason, no exexcutor (defaults to bot)
+            let case_id = pool.add_mod_action(
+                "mute",
+                guild.id.0,
+                &msg.author,
+                Some("User exceeded mention limit. (10)"),
+                true,
+                None,
+            ).case_id;
+
+            // add the mute role
+            if let Err(e) = member.add_role(mute_role as u64) {
+                error!(
+                    "Error while adding auto mute role exceeding mention limit: {}",
+                    e
+                );
+
+
+                // remove pending action if mute failed
+                pool.remove_mod_action(guild.id.0, &msg.author, case_id);
+            }
+        }
+    }
+}
--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,12 +1,11 @@
 // pub mod roles;
 // pub mod bans;
 // pub mod gallery;
-// pub mod antispam;
-// pub mod gallery;
 // pub mod mute;
-pub mod levels;
 #[macro_use]
 pub mod macros;
+
+pub mod levels;
 pub mod util;
 pub mod random_hi;
 pub mod join_leave_message;
@@ -14,3 +13,4 @@ pub mod mod_log;
 pub mod notifications;
 pub mod user_info_activity;
 pub mod invite_guard;
+pub mod anti_spam;
