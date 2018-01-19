b2e3bd8f7efb68d65832f6c4eb601da3ec65f817
drklee3 <dlee3@scu.edu>
2018-01-19 09:48:17 -0800

    add mute_evasion remute mod_log entry

--- src/plugins/mute_evasion.rs
+++ src/plugins/mute_evasion.rs
@@ -7,18 +7,29 @@ use utils::config::get_pool;
 
 pub fn on_guild_member_addition(ctx: &Context, guild_id: &GuildId, member: &mut Member) {
     let pool = get_pool(&ctx);
-    let user_id = member.user.read().id.0;
+    let user = member.user.read().clone();
 
-    if pool.should_mute(user_id, guild_id.0) {
+    if pool.should_mute(user.id.0, guild_id.0) {
         let config = pool.get_guild_config(guild_id.0);
 
         if let Some(role) = config.mute_role {
+            let case_id = pool.add_mod_action(
+                "mute",
+                guild_id.0,
+                &user,
+                Some("User left with a mute role."),
+                true,
+                None,
+            ).case_id;
+
             if let Err(e) = member.add_role(role as u64) {
                 warn!("[plugin:mute_evasion] Error while adding mute role: {}", e);
+                // remove failed mod entry
+                pool.remove_mod_action(guild_id.0, &user, case_id);
             }
         }
 
-        pool.delete_mute(user_id, guild_id.0)
+        pool.delete_mute(user.id.0, guild_id.0)
     }
 }
 
