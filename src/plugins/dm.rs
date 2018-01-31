dcdbb86a42c63f8440ba339ec61444a3e5175ab4
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-01-30 22:10:30 -0800

    ignore own messages when notifying of dms

--- src/plugins/dm.rs
+++ src/plugins/dm.rs
@@ -1,9 +1,17 @@
 use serenity::model::channel::Message;
 use serenity::prelude::Context;
+use serenity::CACHE;
 
 use database::ConnectionPool;
 
 pub fn on_message(_ctx: &Context, _pool: &ConnectionPool, msg: &Message) {
+    let cache = CACHE.read();
+
+    // return if self, prevents messages by self
+    if msg.author.id.0 == cache.user.id.0 {
+        return;
+    }
+
     if msg.is_private() {
         let s = format!("DM from {} ({}):\nMessage: {}\nAttachments: {:?}", msg.author.tag(),
             msg.author.id, msg.content, msg.attachments.iter().map(|x| &x.url).collect::<Vec<&String>>());
