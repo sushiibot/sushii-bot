36e2d7974cdbff658b199cbee86f594a98424fa9
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-02-02 21:09:01 -0800

    fix fm track image and timestamp, add message when saving username

--- src/commands/search/lastfm.rs
+++ src/commands/search/lastfm.rs
@@ -16,9 +16,13 @@ command!(fm(ctx, msg, args) {
     let fm_key = env::var("LASTFM_KEY").expect("Expected LASTFM_KEY to be set in environment");
     let pool = get_pool(&ctx);
 
+    let _ = msg.channel_id.broadcast_typing();
+
     let username_or_set = args.full();
+    let mut saved = false;
 
     let username = if username_or_set.starts_with("set ") {
+        saved = true;
         // save to db
         pool.set_lastfm_username(msg.author.id.0, &username_or_set.replace("set ", ""));
 
@@ -53,22 +57,45 @@ command!(fm(ctx, msg, args) {
     let last_track_album = data.pointer("/recenttracks/track/0/album/#text").and_then(|x| x.as_str()).unwrap_or("N/A");
     let last_track_url = data.pointer("/recenttracks/track/0/url").and_then(|x| x.as_str()).unwrap_or("https://www.last.fm");
     // default blank image for fallback
-    let last_track_image = data.pointer("/recenttracks/track/0/image/2/#text").and_then(|x| x.as_str()).unwrap_or("https://i.imgur.com/oYm77EU.jpg");
+    let last_track_image = {
+        let img = data.pointer("/recenttracks/track/0/image/2/#text").and_then(|x| x.as_str()).unwrap_or("");
+
+        if img.is_empty() {
+            "https://i.imgur.com/oYm77EU.jpg"
+        } else {
+            img
+        }
+    };
 
     let last_track_time = data.pointer("/recenttracks/track/0/date/uts").and_then(|x| x.as_i64()).unwrap_or(0);
     let last_track_timestamp = NaiveDateTime::from_timestamp(last_track_time, 0).format("%Y-%m-%dT%H:%M:%S");
 
-    let _ = msg.channel_id.send_message(|m| m
-        .embed(|e| e
-            .author(|a| a
+    let _ = msg.channel_id.send_message(|m| {
+        let mut m = m;
+
+        if saved {
+            m = m.content(get_msg!("info/fm_saved_username"));
+        }
+
+        m.embed(|e| {
+            let mut e = e;
+
+            e = e.author(|a| a
                 .name(username)
+                .url(&format!("https://www.last.fm/user/{}", username))
+                .icon_url("https://i.imgur.com/C7u8gqg.jpg")
             )
             .color(0xb90000)
             .field("Song", format!("[{}]({})", last_track_name, last_track_url), true)
             .field("Album", last_track_album, true)
-            .thumbnail(last_track_image)
             .field("Artist", last_track_artist, true)
-            .timestamp(last_track_timestamp.to_string())
-        )
-    );
+            .thumbnail(last_track_image);
+
+            if last_track_time != 0 {
+                e = e.timestamp(last_track_timestamp.to_string());
+            }
+
+            e
+        })
+    });
 });
