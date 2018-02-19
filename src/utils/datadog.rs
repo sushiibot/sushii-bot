91261bdd38641edaba85251d4eb11eda83766c3a
drklee3 <dlee3@scu.edu>
2018-02-18 21:00:39 -0800

    check env var for datadog client address before falling back to defaults

--- src/utils/datadog.rs
+++ src/utils/datadog.rs
@@ -1,7 +1,11 @@
 use dogstatsd::{Client, Options};
+use std::env;
 	
 lazy_static! {
-    pub static ref DOGO: Client = Client::new(Options::default()).unwrap();
+    pub static ref DOGO: Client = Client::new(Options::new(
+        &env::var("DD_FROM_ADDR").unwrap_or("127.0.0.1:8126".to_owned()),
+        &env::var("DD_TO_ADDR").unwrap_or("127.0.0.1:8125".to_owned()), "")
+    ).unwrap();
 }
 
 fn vec() -> Vec<String> {
