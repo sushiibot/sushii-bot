d9f16c299d1665a3d813618b41fedeb368c00ae2
drklee3 <dlee3@scu.edu>
2018-03-02 18:19:47 -0800

    respond with help text if direct messaged with help/info/about

--- src/plugins/dm.rs
+++ src/plugins/dm.rs
@@ -13,8 +13,19 @@ pub fn on_message(_ctx: &Context, _pool: &ConnectionPool, msg: &Message) {
     }
 
     if msg.is_private() {
+        let content = &msg.content;
+
+        // check if user is searching for help or info
+        if content == "help" ||
+            content == "info" ||
+            content == "about" {
+
+            let _ = msg.channel_id.say("Hi!  You can find information and a list of commands here: <https://sushii.xyz>\n\
+                The default commands prefix is `-` if you want to try any commands here!");
+        }
+
         let s = format!("DM from {} ({}):\nMessage: {}\nAttachments: {:?}", msg.author.tag(),
-            msg.author.id, msg.content, msg.attachments.iter().map(|x| &x.url).collect::<Vec<&String>>());
+            msg.author.id, content, msg.attachments.iter().map(|x| &x.url).collect::<Vec<&String>>());
         info_discord!(&s);
     }
 }
