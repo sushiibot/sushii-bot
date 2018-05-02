0a453bc9e38d4159d6b0bdf0f6a270510cf2c774
drklee3 <dlee3@scu.edu>
2018-05-02 14:14:36 -0700

    Zero pad roleinfo color and use full args

--- src/commands/guild/roles.rs
+++ src/commands/guild/roles.rs
@@ -11,10 +11,11 @@ command!(roleinfo(_ctx, msg, args) {
         None => return Err(CommandError::from(get_msg!("error/no_guild"))),
     };
 
-    let search = match args.single::<String>() {
-        Ok(val) => val,
-        Err(_) => return Err(CommandError::from(get_msg!("guild/roles/error/no_role_name"))),
-    };
+    let search = args.full();
+
+    if search.is_empty() {
+        return Err(CommandError::from(get_msg!("guild/roles/error/no_role_name")));
+    }
 
     let role = match guild.roles.values().find(|ref x| x.name.to_lowercase() == search.to_lowercase()) {
         Some(val) => val,
@@ -40,7 +41,7 @@ command!(roleinfo(_ctx, msg, args) {
             .field("Hoisted", hoisted, true)
             .field("Mentionable", mentionable, true)
             .field("Position", &role.position.to_string(), true)
-            .field("Color", &format!("#{:X}", role.colour.0), true)
+            .field("Color", &format!("#{:06X}", role.colour.0), true)
             .field("Permissions", &role.permissions.bits().to_string(), true)
         )
     );
