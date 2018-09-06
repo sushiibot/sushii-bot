92c23857fe48efc2016c6764fb38fdc552592dd3
drklee3 <dlee3@scu.edu>
2018-09-06 14:07:27 -0700

    Increase golden fishy rate, decrease max fish

--- src/database.rs
+++ src/database.rs
@@ -635,11 +635,11 @@ impl ConnectionPool {
         let mut rng = thread_rng();
 
         // 1% chance of getting golden fishy == 80-150 fishies?
-        let golden_range = Range::new(1, 100);
+        let golden_range = Range::new(1, 60);
         let is_golden = golden_range.ind_sample(&mut rng) == 1;
 
         let new_fishies: i64 = if is_golden {
-            let between = Range::new(80, 150);
+            let between = Range::new(80, 120);
             between.ind_sample(&mut rng)
         } else if is_self {
             let between = Range::new(5, 20);
