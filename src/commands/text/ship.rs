f93d00d43c7af5ef04bebb8a9444a980efe1ff45
drklee3 <dlee3@scu.edu>
2018-09-13 19:18:42 -0700

    Use hash for ships

--- src/commands/text/ship.rs
+++ src/commands/text/ship.rs
@@ -1,9 +1,10 @@
 use serenity::framework::standard::CommandError;
 
 use chrono::Utc;
+use chrono::Datelike;
 use chrono::Timelike;
-use regex::Regex;
-use rand::{thread_rng, Rng};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 const EMOJIS: &[&str] = &[
     ":nauseated_face:",
@@ -33,32 +34,23 @@ command!(ship(_ctx, msg, args) {
         return Err(CommandError::from(get_msg!("text/ship/error/no_ship_given")));
     }
 
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"<@(\d+)>").unwrap();
-    }
-
-    let mut rng = thread_rng();
-    let count = ship_str.matches("\u{200b}").count();
-
-    let mut percentage = RE
-        .captures_iter(&ship_str)
-        .fold(0, |sum, capture| sum + (capture
-            .get(1) // id
-            .and_then(|group| group // id capture group
-                .as_str()
-                .parse::<usize>()
-                .ok()
-            )
-            .unwrap_or(0) % 100) // sum all ids % 100
-        );
+    // hash string
+    let mut hasher = DefaultHasher::new();
+    ship_str.hash(&mut hasher);
+    let h = hasher.finish() as usize;
     
-    percentage = (percentage * Utc::now().hour() as usize) % 101; // multiply by current hour
+    let mut percentage = h % 1000;
 
-    if percentage == 0 { // no ids found
-        percentage = rng.gen_range(0, 101); // inclusive low, exclusive high
-    }    
+    // multiply by hour / ordinal to make static per hour
+    let now = Utc::now();
+    percentage = (
+        percentage *
+            (now.hour() + 1) as usize *
+            now.ordinal() as usize
+        ) % 101; // multiply by current hour
 
     // definitely not rigged
+    let count = ship_str.matches("\u{200b}").count();
     if count == 1 {
         percentage = 100;
     } else if count == 2 {
@@ -90,6 +82,9 @@ command!(ship(_ctx, msg, args) {
         .embed(|e| e
             .colour(0x3498db)
             .description(&response)
+            .footer(|f| f
+                .text("% resets on each hour")
+            )
         )
     );
 });
