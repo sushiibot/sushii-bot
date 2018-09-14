e03600cd72193f4f3f125a292147212a45aff1f6
drklee3 <dlee3@scu.edu>
2018-09-13 17:03:36 -0700

    Keep ship with mentions static per hour

--- src/commands/text/ship.rs
+++ src/commands/text/ship.rs
@@ -1,5 +1,8 @@
 use serenity::framework::standard::CommandError;
 
+use chrono::Utc;
+use chrono::Timelike;
+use regex::Regex;
 use rand::{thread_rng, Rng};
 
 const EMOJIS: &[&str] = &[
@@ -30,15 +33,36 @@ command!(ship(_ctx, msg, args) {
         return Err(CommandError::from(get_msg!("text/ship/error/no_ship_given")));
     }
 
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"<@(\d+)>").unwrap();
+    }
+
     let mut rng = thread_rng();
     let count = ship_str.matches("\u{200b}").count();
 
-    let percentage = if count == 1 {
-        100
+    let mut percentage = RE
+        .captures_iter(&ship_str)
+        .fold(0, |sum, capture| sum + (capture
+            .get(1) // id
+            .and_then(|group| group // id capture group
+                .as_str()
+                .parse::<usize>()
+                .ok()
+            )
+            .unwrap_or(0) % 100) // sum all ids % 100
+        );
+    
+    percentage = (percentage * Utc::now().hour() as usize) % 101; // multiply by current hour
+
+    if percentage == 0 { // no ids found
+        percentage = rng.gen_range(0, 101); // inclusive low, exclusive high
+    }    
+
+    // definitely not rigged
+    if count == 1 {
+        percentage = 100;
     } else if count == 2 {
-        0
-    } else {
-        rng.gen_range(0, 101) // inclusive low, exclusive high
+        percentage = 0;
     };
 
     let length = percentage / 10;
