76639872931a11d506d569710cc3e192c69f02cc
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-09-17 20:53:40 -0700

    Prevent clone of guild in guildinfo

--- src/commands/guild/guild_info.rs
+++ src/commands/guild/guild_info.rs
@@ -4,18 +4,18 @@ use serenity::model::user::OnlineStatus;
 command!(guild_info(_ctx, msg, _args) {
     // get the guild
     let guild = match msg.guild() {
-        Some(val) => val.read().clone(),
+        Some(g) => g,
         None => return Err(CommandError::from(get_msg!("error/no_guild"))),
     };
 
-    let owner_tag = match guild.owner_id.to_user() {
+    let owner_tag = match guild.read().owner_id.to_user() {
         Ok(val) => val.tag(),
         Err(_) => "N/A".into(),
     };
 
     let guild_presences = {
         //Online, Idle, DoNotDisturb, Offline
-        let presences = guild.presences.values().fold((0, 0, 0), |mut acc, x| {
+        let presences = guild.read().presences.values().fold((0, 0, 0), |mut acc, x| {
             match x.status {
                 OnlineStatus::Online => acc.0 += 1,
                 OnlineStatus::Idle => acc.1 += 1,
@@ -32,7 +32,7 @@ command!(guild_info(_ctx, msg, _args) {
 
     let voice_states = {
         // self_deaf, self_mute
-        let states = guild.voice_states.values().fold((0, 0), |mut acc, x| {
+        let states = guild.read().voice_states.values().fold((0, 0), |mut acc, x| {
             if x.self_deaf {
                 acc.0 += 1;
             }
@@ -45,29 +45,29 @@ command!(guild_info(_ctx, msg, _args) {
         });
 
         format!("{} total / {} deafened / {} muted",
-            guild.voice_states.len(), states.0, states.1)
+            guild.read().voice_states.len(), states.0, states.1)
     };
 
     let _ = msg.channel_id.send_message(|m| m
         .embed(|e| e
             .author(|a| a
-                .name(&guild.name)
-                .url(&guild.icon_url().unwrap_or_else(|| "N/A".into()))
-                .icon_url(&guild.icon_url().unwrap_or_else(|| "N/A".into()))
+                .name(&guild.read().name)
+                .url(&guild.read().icon_url().unwrap_or_else(|| "N/A".into()))
+                .icon_url(&guild.read().icon_url().unwrap_or_else(|| "N/A".into()))
             )
-            .thumbnail(&guild.icon_url().unwrap_or_else(|| "N/A".into()))
-            .field("Owner", &format!("{} ({})", owner_tag, guild.owner_id), true)
-            .field("Channels", &guild.channels.len().to_string(), true)
-            .field("Emojis", &guild.emojis.len().to_string(), true)
-            .field("Explicit Content Filter", &format!("{:?}", guild.explicit_content_filter), true)
-            .field("Member Count", &guild.member_count.to_string(), true)
-            .field("Region", &guild.region, true)
-            .field("Roles", &guild.roles.len().to_string(), true)
-            .field("Verification Level", &format!("{:?}", guild.verification_level), true)
+            .thumbnail(&guild.read().icon_url().unwrap_or_else(|| "N/A".into()))
+            .field("Owner", &format!("{} ({})", owner_tag, guild.read().owner_id), true)
+            .field("Channels", &guild.read().channels.len().to_string(), true)
+            .field("Emojis", &guild.read().emojis.len().to_string(), true)
+            .field("Explicit Content Filter", &format!("{:?}", guild.read().explicit_content_filter), true)
+            .field("Member Count", &guild.read().member_count.to_string(), true)
+            .field("Region", &guild.read().region, true)
+            .field("Roles", &guild.read().roles.len().to_string(), true)
+            .field("Verification Level", &format!("{:?}", guild.read().verification_level), true)
             .field("Presences", &guild_presences, false)
             .field("Voice States", &voice_states, false)
             .footer(|f| f
-                .text(&format!("Guild ID: {}", &guild.id.0))
+                .text(&format!("Guild ID: {}", &guild.read().id.0))
             )
         )
     );
