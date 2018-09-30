a78114bb03942be080e712412e64c3897995cab9
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-09-30 09:34:06 -0700

    Azure deployment?

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# retain old build
+mv /home/tzuwy/discord/x86_64-unknown-linux-gnu /home/tzuwy/discord/x86_64-unknown-linux-gnu.bak
+
+# rename new build
+mv /home/tzuwy/discord/sushii /home/tzuwy/discord/x86_64-unknown-linux-gnu
