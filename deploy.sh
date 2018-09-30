f6b8dc186abec38b44cafc8042d40bdf985aca57
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2018-09-30 14:33:32 -0700

    Revert "Azure deployment?"
    This reverts commit 85726a1da1cb54dff1ae3ba78f16c9ffbd3ab93c.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# retain old build
-mv /home/tzuwy/discord/x86_64-unknown-linux-gnu /home/tzuwy/discord/x86_64-unknown-linux-gnu.bak
-
-# rename new build
-mv /home/tzuwy/discord/sushii /home/tzuwy/discord/x86_64-unknown-linux-gnu
